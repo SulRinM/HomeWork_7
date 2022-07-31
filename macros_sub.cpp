d8f72c78166d7548c4f6c77de942521add72d431
SulRinM <108032806+SulRinM@users.noreply.github.com>
2022-08-01 00:39:18 +0300

    first commit

---
+++ macros_sub.cpp
@@ -0,0 +1,32 @@
+﻿/*
+    В этом задании мы создадим простой макрос
+
+	Ваша задача - определить макрос SUB, который должен вычитать числа друг из друга 
+	и используется следующим образом:
+
+	int main (int argc, char** argv)
+	{
+		int a = 6;
+		int b = 5;
+		int c = 2;
+		std::cout << SUB(a, b) << std::endl;
+		std::cout << SUB(a, b) * c << std::endl;
+		std::cout << SUB(a, b + c) * c << std::endl;
+	}
+*/
+#include <iostream>
+
+#define SUB(a, b) ((a) - (b))
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+    return 0;
+}
+
